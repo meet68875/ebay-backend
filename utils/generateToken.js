3b85f608afaaa78284205812ab675434b622aac7
my-first-git-project <meet68875@gmail.com>
2025-08-14 21:43:43 +0530

    change expire time

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -1,5 +1,5 @@
 import jwt from "jsonwebtoken";
 
 export const generateToken = (id) => {
-  return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "10m" });
+  return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "20m" });
 };
